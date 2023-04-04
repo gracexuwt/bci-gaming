297b1a6e6be1f4ff8be1b1486149fee5364cdc41
682x <ryanyan@rogers.com>
2023-04-03 20:29:41 -0400

    Create character and player class, add collisions (working for ground only)

---
+++ bci-game/Assets/Player.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{   
+    
+
+    public override float[] GetInput() {
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
+        return new float[] {hInput, vInput};
+    }
+
+
+}
+
