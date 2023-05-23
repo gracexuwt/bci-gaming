d5457c4b01314c31bebe1adaa1c3e05f2a867e1f
Christopher Fong <79167402+christopherfong115@users.noreply.github.com>
2023-05-22 20:18:39 -0400

    Add files via upload

--- bci-game/Assets/Scripts/BoardData.cs
+++ bci-game/Assets/Scripts/BoardData.cs
@@ -10,6 +10,7 @@ public class BoardData : MonoBehaviour
 {
     private BoardShim board_shim = null;
     private int sampling_rate = 0;
+    private double[,] data;
 
     // Start is called before the first frame update
     void Start()
@@ -20,9 +21,9 @@ public class BoardData : MonoBehaviour
             BoardShim.enable_dev_board_logger();
 
             BrainFlowInputParams input_params = new BrainFlowInputParams();
-            input_params.ip_port = 6677;
-            input_params.ip_address = "225.1.1.1";
-            int board_id = (int)BoardIds.CYTON_BOARD;
+            //input_params.ip_port = 6677;
+            //input_params.ip_address = "225.1.1.1";
+            int board_id = (int)BoardIds.SYNTHETIC_BOARD;
             board_shim = new BoardShim(board_id, input_params);
             board_shim.prepare_session();
             board_shim.start_stream(450000, "file://brainflow_data.csv:w");
@@ -44,9 +45,18 @@ public class BoardData : MonoBehaviour
             return;
         }
         int number_of_data_points = sampling_rate * 4;
+        //Debug.Log("Data poitns: " + number_of_data_points);
         double[,] data = board_shim.get_current_board_data(number_of_data_points);
+        int[] eeg_channels = BoardShim.get_eeg_channels((int)BoardIds.SYNTHETIC_BOARD);
         // check https://brainflow.readthedocs.io/en/stable/index.html for api ref and more code samples
-        Debug.Log("Num elements: " + data.GetLength(1));
+        //Debug.Log("Num elements: " + data.GetLength(1));
+        for (int i = 0; i < data.GetLength(0); i++)
+        {
+            for (int j = 0; j < data.GetLength(1); j++)
+            {
+                Debug.Log(data[i, j]);
+            }
+        }
     }
 
     // you need to call release_session and ensure that all resources correctly released
