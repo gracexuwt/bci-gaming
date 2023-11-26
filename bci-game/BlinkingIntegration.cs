ad2d3c3cb8bb5ca0fe2c8e5fe74b97f7384fc7b4
Chevin Jeon <chevinjeon@gmail.com>
2023-11-26 17:43:13 -0500

    Updated C# code

--- bci-game/BlinkingIntegration.cs
+++ bci-game/BlinkingIntegration.cs
@@ -1,31 +1,83 @@
-using UnityEngine;
-using uPLibrary.Networking.M2Mqtt;
-using uPLibrary.Networking.M2Mqtt.Messages;
-using System.Text;
+using System;
+using brainflow;
+using brainflow.math;
 
-public class EEGVisualization : MonoBehaviour
+class BCIApplication
 {
-    private MqttClient client;
-    private GameObject eegVisualizationObject;
-
-    void Start()
+    static void Main(string[] args)
     {
-        // Initialize MQTT client
-        client = new MqttClient("mqtt_broker_host");
-        client.MqttMsgPublishReceived += Client_MqttMsgPublishReceived;
-        client.Connect("unity_client");
-        client.Subscribe(new string[] { "eeg_topic" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+        BoardShim.enable_dev_board_logger();
 
-        // Create or load EEG visualization GameObject
-        eegVisualizationObject = GameObject.Find("EEGVisualization"); // Adjust GameObject name as needed
-    }
+        BrainFlowInputParams inputParams = new BrainFlowInputParams();
+        int boardId = (int)BoardIds.CROWN_BOARD; // Or other BoardIds as needed
+        inputParams.serial_number = "9bb2e2fad5668286a4b4f407002b4359";
+        int samplingRate = BoardShim.get_sampling_rate(boardId);
+        int timeThres = 100;
+        double maxVal = -100000000000;
+        double valsMean = 0;
+        int numSamples = 5000;
+        int samples = 0;
 
-    private void Client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
-    {
-        string eegData = Encoding.UTF8.GetString(e.Message);
+        BoardShim board = new BoardShim(boardId, inputParams);
+        board.prepare_session();
+        board.start_stream(450000);
+        Console.WriteLine("Starting calibration");
+        System.Threading.Thread.Sleep(5000);
+        double[,] data = board.get_board_data();
+        Console.WriteLine("Start blinking");
 
-        // Parse and process EEG data here
-        // Update EEG visualization GameObject based on data received
-        // This part depends on your EEG data format and visualization design
+        // Calibration
+        while (samples < numSamples)
+        {
+            data = board.get_board_data();
+            int channelLength = data.GetLength(1); // Assuming channel 1 is the EEG data
+            if (channelLength > 0)
+            {
+                double[] channelData = data.GetRow(1);
+                DataFilter.perform_rolling_filter(channelData, 2, (int)AggOperations.MEAN);
+                for (int i = 0; i < channelLength; i++)
+                {
+                    valsMean += channelData[i] / numSamples;
+                }
+                samples += channelLength;
+                maxVal = Math.Max(maxVal, channelData.Max());
+            }
+        }
+
+        double blinkThres = 0.5 * Math.Pow((maxVal - valsMean), 2);
+        Console.WriteLine($"Mean value: {valsMean}");
+        Console.WriteLine($"Max value: {maxVal}");
+        Console.WriteLine($"Threshold: {blinkThres}");
+        Console.WriteLine("CALIBRATION DONE START PLAYING");
+
+        long prevTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
+        // Main loop
+        while (true)
+        {
+            data = board.get_board_data();
+            int channelLength = data.GetLength(1);
+            if (channelLength > 0)
+            {
+                double[] channelData = data.GetRow(1);
+                DataFilter.perform_rolling_filter(channelData, 2, (int)AggOperations.MEAN);
+                if ((DateTimeOffset.Now.ToUnixTimeMilliseconds() - timeThres) > prevTime)
+                {
+                    prevTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+                    foreach (double element in channelData)
+                    {
+                        if (Math.Pow((element - valsMean), 2) >= blinkThres)
+                        {
+                            Console.WriteLine("WOOO BLINK DETECTED");
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        // Cleanup
+        board.stop_stream();
+        board.release_session();
     }
 }
