ed6ed8c4dc3990ceec785564298b65f5bf479962
kimchiloof <kevin.2005.ge@gmail.com>
2023-11-28 22:29:30 -0500

    Added IDamageable.cs to test knockback, Fixed flipping

--- bci-game/Assets/Scripts/Entity/Player/Player.cs
+++ bci-game/Assets/Scripts/Entity/Player/Player.cs
@@ -2,20 +2,35 @@ namespace Entity.Player
 {
     using UnityEngine;
     using Entity.Interfaces;
+    using Entity.Utils;
 
     [RequireComponent(typeof(PlayerMovement))]
-    public class Player : MonoBehaviour, IPositionTrackable
-    {   
-        private PlayerMovement playerMovement;
+    public class Player : MonoBehaviour, IPositionTrackable, IDamageable
+    {
+        private Rigidbody2D body;
+        private CharacterSoundController soundController;
         
+        private void Awake()
+        {
+            body = GetComponent<Rigidbody2D>();
+            soundController = GetComponent<CharacterSoundController>();
+        }
+        
+        // IPositionTrackable implementation
         public Vector2 GetPosition()
         {
             return transform.position;
         }
-        
-        private void Awake()
+
+        // IDamageable implementation
+        public void Damage(float damageAmount, Vector2 knockbackDirection, float knockbackForce)
         {
-            playerMovement = GetComponent<PlayerMovement>();
+            // health.health -= damageAmount;
+            // if(health.health < 0) Die();
+            soundController.PlayHurtSound();
+
+            Vector2 knockback = knockbackDirection.normalized * knockbackForce;
+            body.AddForce(knockback, ForceMode2D.Impulse);
         }
     }
 }
