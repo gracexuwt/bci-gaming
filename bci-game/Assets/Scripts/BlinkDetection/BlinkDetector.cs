e4fb51e251838527b9f81e407f8c4f3623e40370
alexisbader <79996302+alexisbader@users.noreply.github.com>
2024-02-24 19:01:50 -0500

    Added neurosity connection

---
+++ bci-game/Assets/Scripts/BlinkDetection/BlinkDetector.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Notion.Unity.Example
+{
+    public class BlinkDetector : MonoBehaviour
+    {
+        private decimal threshold;
+        private decimal meanValue;
+        private decimal maxValue;
+        int sampleCount = 0;
+
+
+        public void Calibrate(List<decimal> rawData)
+        {
+            decimal sum = 0;
+            maxValue = decimal.MinValue;
+
+            foreach (var value in rawData)
+            {
+                sum += value;
+                sampleCount++;
+
+                if (value > maxValue)
+                {
+                    maxValue = value;
+                }
+            }
+
+            meanValue = sum / sampleCount;
+            threshold = 0.4m * (decimal)Math.Pow((double)(maxValue - meanValue), 2.0);
+
+            Debug.Log(meanValue);
+            Debug.Log(threshold);
+        }
+
+        public void DetectBlink(decimal[] rawData)
+        {
+            foreach (var value in rawData)
+            {
+                decimal squaredDifference = (decimal)Math.Pow((double)(value - meanValue), 2.0);
+
+
+                // Debug.Log("Thershold" + squaredDifference);
+                if (squaredDifference >= threshold)
+                {
+                    Debug.Log("Blink Detected!");
+                    return; // If you only want to detect one blink in the current data
+                }
+        }
+        }
+    }
+}
---
+++ bci-game/Assets/Scripts/Notion-Unity/Handlers/BrainwavesRawHandler.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace Notion.Unity.Example
+{
+    public class BrainwavesRawHandler : IMetricHandler
+    {
+        int count = 0;
+        public Metrics Metric => Metrics.Brainwaves;
+        public string Label => "raw";
+        BlinkDetector blinkDetector = new BlinkDetector();
+        List<decimal> epochCalibration = new List<decimal>();
+        int CALIBRATIONTIME = 100;
+
+        private readonly StringBuilder _builder;
+
+        public BrainwavesRawHandler()
+        {
+            _builder = new StringBuilder();
+        } 
+
+        public void Handle(string metricData)
+        {
+            Epoch epoch = JsonConvert.DeserializeObject<Epoch>(metricData);
+
+            _builder.AppendLine("Handling Raw Brainwaves")
+                .Append("Label: ").AppendLine(epoch.Label)
+                .Append("Notch Frequency: ").AppendLine(epoch.Info.NotchFrequency)
+                .Append("Sampling Rate: ").AppendLine(epoch.Info.SamplingRate.ToString())
+                .Append("Start Time: ").AppendLine(epoch.Info.StartTime.ToString())
+                .Append("Channel Names: ").AppendLine(string.Join(", ", epoch.Info.ChannelNames));
+
+            // Debug.Log("Epoch data");
+            // Debug.Log(JsonConvert.SerializeObject(epoch.Data)); // Log serialized JSON string for the data
+            // Debug.Log(_builder.ToString());
+            // Debug.Log(JsonConvert.SerializeObject(epoch.Data)); // Log serialized JSON string for the data
+            _builder.Clear();
+            
+            if(count < CALIBRATIONTIME) {
+                Debug.Log("Start collecting calibration data");
+                count = count + 1;
+                for (int i = 0; i < epoch.Data[0].Length; i++)
+                {
+                    epochCalibration.Add(epoch.Data[0][i]);
+                    Debug.Log(epochCalibration);
+                }
+            }
+            else if (count == CALIBRATIONTIME) 
+            {
+                Debug.Log(epochCalibration);
+                Debug.Log("Calibrating");
+                count = count + 1;
+                blinkDetector.Calibrate(epochCalibration);
+            }
+            else if (count > CALIBRATIONTIME)
+            {
+                Debug.Log("Calibration finished - start blinking");
+                blinkDetector.DetectBlink(epoch.Data[0]);
+            }
+        }
+    }
+}
