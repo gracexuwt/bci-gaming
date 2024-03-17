dd4dad2251546bb1064f1deaeae420e84b15316a
gracexuwt <gracexuwt1126@gmail.com>
2024-03-17 02:22:37 -0400

    Animation delay fixed

--- bci-game/Assets/Scripts/Entity/Player/PlayerAttackRange.cs
+++ bci-game/Assets/Scripts/Entity/Player/PlayerAttackRange.cs
@@ -19,6 +19,7 @@ namespace Entity.Player
         [SerializeField] private AudioClip[] rangeAttackSounds;
         [SerializeField, Range(0f, 1f)] private float rangeAttackVolume = 0.8f;
         
+        private float animationTime = 0.5f;
         private static readonly int IsRanged = Animator.StringToHash("isRanged");
 
         private void Awake()
@@ -39,14 +40,17 @@ namespace Entity.Player
         {
             soundController.PlaySound(rangeAttackSounds, rangeAttackVolume);
             animator.SetTrigger(IsRanged);
-            Instantiate(rangeProjectile, firePoint.position, transform.localScale.x > 0 ? Quaternion.identity : Quaternion.Euler(0, 180, 0));
-            
-            StartCoroutine(AttackCooldown(1f / rangeAttackSpeed));
+
+            canFire = false;
+            StartCoroutine(Attack(1f / rangeAttackSpeed));
         }
         
-        private IEnumerator AttackCooldown(float duration)
+        private IEnumerator Attack(float duration)
         {
-            canFire = false;
+            yield return new WaitForSeconds(animationTime);
+            //shoot after animation finishes playing
+            Instantiate(rangeProjectile, firePoint.position, transform.localScale.x > 0 ? Quaternion.identity : Quaternion.Euler(0, 180, 0));
+            //attack cooldown
             yield return new WaitForSeconds(duration);
             canFire = true;
         }
